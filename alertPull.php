7f1f85a34867cb4fcead01b9c412e6cd47599e0b
bjpatterson <bjpatterson@bsu.edu>
2016-09-30 17:45:47 -0400

    modify php to localize query
    for efficiency

--- alertPull.php
+++ alertPull.php
@@ -1,29 +1,33 @@
 <?php
 	error_reporting(0);
 	include("dbConnect.php");
+	$lat = (float)$_GET['Lat'];
+	$long = (float)$_GET['Lon'];
 	$response = array();
 
- 	$sql = "SELECT * FROM AlertEntry";
+ 	$sql = "SELECT * FROM AlertEntry as A WHERE SQRT(POW(A.Latitude-$lat,2) + POW(A.Longitude-$long,2))*80 <= A.Radius;"; // a crude approximation to narrow down results
+	
  	$result = mysql_query($sql) or die("Did not run the query");
+	
  	if(mysql_num_rows($result)>0){
-           $response["orders"] = array();
-		   while ($row = mysql_fetch_array($result)) {
-            // temp user array
-            $alert = array();
-            $alert["id"] = $row["Id"];
-            $alert["DescribeEvents"] = $row["DescribeEvents"];
+		$response["orders"] = array();
+		while ($row = mysql_fetch_array($result)) {
+			// temp user array
+			$alert = array();
+			$alert["id"] = $row["Id"];
+			$alert["DescribeEvents"] = $row["DescribeEvents"];
 			$alert["Latitude"] = $row["Latitude"];
-            $alert["Longitude"] = $row["Longitude"];
+			$alert["Longitude"] = $row["Longitude"];
 			$alert["CurrentTimeEvents"] = $row["CurrentTimeEvents"];
-            $alert["Level"] = $row["Level"];
+			$alert["Level"] = $row["Level"];
 			$alert["Radius"] = $row["Radius"];
 			
         
             // push ordered items into response array 
             array_push($response["orders"], $alert);
-           }
-      // success
-     $response["success"] = 1;
+		}	
+		// success
+		$response["success"] = 1;
 	}
 	else {
     // order is empty 
