c9b860e1472d443827f1747efdfb317bbfdc2e05
bjpatterson <bjpatterson@bsu.edu>
2016-09-30 17:42:34 -0400

    add changes
    so that we can submit the final product

--- AlertPull/app/src/main/java/com/example/apple/alertpull/AlertPullEvents.java
+++ AlertPull/app/src/main/java/com/example/apple/alertpull/AlertPullEvents.java
@@ -10,6 +10,7 @@ import android.location.LocationListener;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -24,7 +25,6 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.maps.model.CircleOptions;
@@ -43,10 +43,10 @@ import org.json.JSONObject;
 
 
 
-public class AlertPullevens extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener,LocationListener {
+public class AlertPullEvents extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener,LocationListener {
 
     private GoogleMap mMap;
-    private static final String BASE_URL = "http://10.64.83.126";
+    private static final String BASE_URL = "http://10.230.120.9";
     private static final String PULL_URL = BASE_URL + "/alertPull.php";
     private static final LatLng BALL_STATE = new LatLng(40.2011, -85.4070);
 
@@ -96,14 +96,11 @@ public class AlertPullevens extends FragmentActivity implements OnMapReadyCallba
 
         // Add a marker in Sydney and move the camera
         geocoder = new Geocoder(this);
-        this.mMap.setOnMarkerClickListener(this);
+        mMap.setOnMarkerClickListener((GoogleMap.OnMarkerClickListener) this);
         mMap.getUiSettings().setZoomControlsEnabled(true);
         initLocation();
         this.zoomToLocation(BALL_STATE, 15);
-//        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
-//                PackageManager.PERMISSION_GRANTED) {
-//            mMap.setMyLocationEnabled(true);
-//        }
+
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED) {
             mMap.setMyLocationEnabled(true);
@@ -114,30 +111,8 @@ public class AlertPullevens extends FragmentActivity implements OnMapReadyCallba
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, degree));
     }
 
-    public void setMarkers(LatLng latLng, double radius, String title, double levelnumber) {
-//        if (marker != null) {
-//            marker.remove();
-//            marker = null;
-//
-//        }
-        MarkerOptions options = new MarkerOptions()
-                .position(latLng)
-                .title(title);
-        marker = mMap.addMarker(options);
-        circle = drawCirle(latLng, radius, levelnumber);
-
-    }
-
-    public void lableMarkers(LatLng latLng) {
-        MarkerOptions options = new MarkerOptions()
-                .position(latLng);
-        marker = mMap.addMarker(options);
-    }
-
     public Circle drawCirle(LatLng latLng, double radius, double levelNumber) {
-//        if (circle != null) {
-//            circle.remove();
-//        }
+
         int color = 0;
         if (levelNumber == 1) {
             color = getResources().getColor(R.color.colorAlertLevel1);
@@ -156,8 +131,11 @@ public class AlertPullevens extends FragmentActivity implements OnMapReadyCallba
                 .strokeWidth(3);
         return mMap.addCircle(options);
     }
+
+
     public void onPull(View view){
-        StringRequest stringRequest = new StringRequest(PULL_URL, new Response.Listener<String>() {
+        String customReq = PULL_URL + "?Lat=" + mLocation.getLatitude() + "&Lon=" + mLocation.getLongitude();
+        StringRequest stringRequest = new StringRequest(customReq, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
 
@@ -168,7 +146,7 @@ public class AlertPullevens extends FragmentActivity implements OnMapReadyCallba
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(AlertPullevens.this,error.getMessage().toString(),Toast.LENGTH_LONG).show();
+                        Toast.makeText(AlertPullEvents.this,error.getMessage().toString(),Toast.LENGTH_LONG).show();
                     }
                 });
         RequestQueue requestQueue = Volley.newRequestQueue(this);
@@ -188,14 +166,12 @@ public class AlertPullevens extends FragmentActivity implements OnMapReadyCallba
                 double radius = Double.valueOf(jsonArray.getJSONObject(i).get(ITEM_RADIUS).toString());
                 double level = Double.valueOf(jsonArray.getJSONObject(i).get(ITEM_LEVEL).toString());
                 String date = (String)jsonArray.getJSONObject(i).get(ITEM_TIME);
-                String event = (String)jsonArray.getJSONObject(i).get(Type);
+                String eventType = (String)jsonArray.getJSONObject(i).get(Type);
                 int id = Integer.valueOf(jsonArray.getJSONObject(i).get("id").toString());
-                AlertEvents bsuEvent = new AlertEvents(id, lat, lng, event, date,level,radius);
-                this.eventList.add(bsuEvent);
-                System.out.println("evenlist is here "+ eventList);
-                System.out.println("bsuevent: "+bsuEvent);
-                LatLng latLng = new LatLng(lat,lng);
-                setMarkers(latLng,radius,event,level);
+                AlertEvents event = new AlertEvents(id, lat, lng, eventType, date,level,radius);
+                this.eventList.add(event);
+                System.out.println("eventlist is here "+ eventList);
+                System.out.println("bsuevent: "+ event);
 
             }
             mMap.clear();
@@ -209,29 +185,16 @@ public class AlertPullevens extends FragmentActivity implements OnMapReadyCallba
     }
     private void addMarkers(Location userLocation) {
         for(AlertEvents e : eventList){
-//            float distance=e.distanceToUser(userLocation);
             Location eventLocation = new Location("");
             eventLocation.setLatitude(e.getLat());
             eventLocation.setLongitude(e.getLng());
-            double levelnumber = e.getLevel();
+            double levelNumber = e.getLevel();
             double radius = e.getRadius();
-//            System.out.println("Distance "+distance);
-//            if(distance<=100){
-//                e.setType("Level 4  "+e.getType());
-//            }
-//            else if(distance<=200){
-//                e.setType("Level 3 "+e.getType());
-//            }
-//            else if(distance<=500){
-//                e.setType("Level 2 "+e.getType());
-//            }
-//            else{
-//                e.setType("Level 1 "+e.getType());
-//            }
+
             LatLng event=new LatLng(e.getLat(),e.getLng());
 
             mMap.addMarker(new MarkerOptions().title(e.getType()).position(event).snippet(e.getDatetime()));
-            circle = drawCirle(event, radius, levelnumber);
+            circle = drawCirle(event, radius, levelNumber);
         }
         LatLng latLng = new LatLng(userLocation.getLatitude(),userLocation.getLongitude());
         mMap.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
@@ -246,7 +209,7 @@ public class AlertPullevens extends FragmentActivity implements OnMapReadyCallba
         } else if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
             provider = LocationManager.NETWORK_PROVIDER;
         } else {
-            Toast.makeText(AlertPullevens.this, "no provider is currently available", Toast.LENGTH_LONG)
+            Toast.makeText(AlertPullEvents.this, "no provider is currently available", Toast.LENGTH_LONG)
                     .show();
             return;
         }
@@ -265,33 +228,11 @@ public class AlertPullevens extends FragmentActivity implements OnMapReadyCallba
         this.mLocation = location;
         System.out.println("My location!!!!"+mLocation);
         this.onPull(null);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLocation.getLatitude(), mLocation.getLongitude()), 15));
     }
     @Override
     public boolean onMarkerClick(final Marker marker){
-//        Location markerLocation = new Location(" ");
-//        markerLocation.setLongitude(marker.getPosition().longitude);
-//        markerLocation.setLatitude(marker.getPosition().latitude);
-//        float radius = this.mLocation.distanceTo(markerLocation);
-//        int color = 0;
-//        if(radius<=100){
-//            color = Color.RED;
-//        }
-//        else if(radius<=200){
-//            color = Color.CYAN;
-//        }
-//        else if(radius<=500){
-//            color = Color.BLUE;
-//        }
-//        else{
-//            color = Color.LTGRAY;
-//        }
-//        if (circle != null){
-//            circle.remove();
-//        }
-//        circle = mMap.addCircle(new CircleOptions().center(marker.getPosition()).
-//                radius(radius).fillColor(color).strokeWidth(1));
-//        marker.showInfoWindow();
+        Log.d("MAP", marker.getTitle() + " Marker Clicked!");
+        marker.showInfoWindow();
         return true;
     }
     @Override
